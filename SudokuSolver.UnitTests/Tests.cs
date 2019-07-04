0c7e13391c6c436f5765421f3f23a4acff2aff57
Alex Kostas <k.alexandros299@gmail.com>
2019-07-04 21:14:11 +0300

    Add Unit Test project

---
+++ SudokuSolver.UnitTests/Tests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace SudokuSolver.UnitTests {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
